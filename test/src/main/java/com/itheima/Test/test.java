b6f20d5929a6d477a464fd315fe2b7dfebf4b7a6
gh <78259254@qq.com>
2018-09-26 23:43:01 +0800

    提交代码

---
+++ test/src/main/java/com/itheima/Test/test.java
@@ -0,0 +1,10 @@
+package com.itheima.Test;
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("-------------------------");
+        Date date = new Date();
+    }
+}
